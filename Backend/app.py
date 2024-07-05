0c68ecb6ce4e1c6d97e4fbf904acb2fc26ed00ab
WorkChirag441 <164389763+WorkChirag441@users.noreply.github.com>
2024-07-05 12:16:02 +0530

    Backend issue solved for finacial analysis

---
+++ Backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({'message': 'Hello from Flask!'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
